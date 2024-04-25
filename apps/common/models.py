cb09a8eebb057029726d1b288298422cc65ff859
Daniil <39118675+TyPaporotnyk@users.noreply.github.com>
2024-04-25 10:32:09 +0300

    Changed uuid to id

--- apps/common/models.py
+++ apps/common/models.py
@@ -1,10 +1,7 @@
-import uuid
-
 from django.db import models
 
 
 class TimedBaseModel(models.Model):
-    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     created_at = models.DateTimeField(
         verbose_name="Дата создания",
         auto_now_add=True,
