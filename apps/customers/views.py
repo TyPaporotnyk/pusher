24881c81e95fb8ddeded28347f07de3c69a29413
Daniil <39118675+TyPaporotnyk@users.noreply.github.com>
2024-06-05 13:45:44 +0300

    Implemented import models from excel file

---
+++ apps/customers/views.py
@@ -0,0 +1,42 @@
+from django.shortcuts import redirect, render
+
+from apps.customers.forms import ExcelUploadForm
+from apps.customers.use_case.excel import import_group_from_excel, import_keyword_from_excel
+
+
+def import_keywords_view(request):
+    if request.method == "POST":
+        form = ExcelUploadForm(request.POST, request.FILES)
+        if form.is_valid():
+            excel_file = request.FILES["excel_file"]
+            import_keyword_from_excel(
+                excel_file,
+            )
+            return redirect("customers:import_success")
+    else:
+        form = ExcelUploadForm()
+
+    return render(
+        request,
+        "customers/load_model_from_file.html",
+        {"form": form},
+    )
+
+
+def import_groups_view(request):
+    if request.method == "POST":
+        form = ExcelUploadForm(request.POST, request.FILES)
+        if form.is_valid():
+            excel_file = request.FILES["excel_file"]
+            import_group_from_excel(
+                excel_file,
+            )
+            return redirect("import_success")
+    else:
+        form = ExcelUploadForm()
+
+    return render(
+        request,
+        "customers/load_model_from_file.html",
+        {"form": form},
+    )
