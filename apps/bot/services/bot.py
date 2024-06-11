7d61b6878c59aac108aaf0dd66281d37f432ec04
Daniil <39118675+TyPaporotnyk@users.noreply.github.com>
2024-06-11 19:05:37 +0300

    Fixed message spliting

--- apps/bot/services/bot.py
+++ apps/bot/services/bot.py
@@ -28,3 +28,38 @@ class BotSenderService:
 
     def send(self, chat_id: int, message: str):
         antiflood(self.bot.send_message, chat_id, number_retries=2, text=message[:4096], parse_mode="HTML")
+
+
+def split_message(text, max_length=4096):
+    parts = []
+    while len(text) > max_length:
+        split_point = text.rfind("\n", 0, max_length)
+        if split_point == -1:
+            split_point = max_length
+        parts.append(text[:split_point])
+        text = text[split_point:]
+    parts.append(text)
+    return parts
+
+
+def send_large_message(bot: TeleBot, user_id: int, message: str, photos: list[str]):
+    # Сначала отправляем фотографии с первой частью сообщения
+    sender_service = BotSenderService(bot)
+    if photos:
+        try:
+            sender_service.send_media(user_id, message, photos)
+            # Удаляем часть сообщения, отправленную с фотографиями
+            message = message[1024:]
+        except Exception as e:
+            logger.warning("Error sending media to bot due to %s", str(e))
+            sender_service.send(user_id, message)
+            return True
+
+    # Если оставшаяся часть сообщения превышает 4096 символов, делим его на части
+    if len(message) > 4096:
+        message_parts = split_message(message)
+        for part in message_parts:
+            sender_service.send(user_id, part)
+    else:
+        sender_service.send(user_id, message)
+    return True
