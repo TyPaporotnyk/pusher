51ed1d72fd7662e0a78f5955b60cfd4f2a1c2e69
Daniil <39118675+TyPaporotnyk@users.noreply.github.com>
2024-06-11 03:04:16 +0300

    Added admin to common models

--- apps/common/admin.py
+++ apps/common/admin.py
@@ -2,7 +2,29 @@ from django.contrib import admin
 
 from apps.common.models import Blacklist, Category, Group, Keyword
 
-admin.site.register(Category)
-admin.site.register(Blacklist)
-admin.site.register(Group)
-admin.site.register(Keyword)
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ("name",)
+    search_fields = ("name",)
+
+
+@admin.register(Blacklist)
+class BlacklistAdmin(admin.ModelAdmin):
+    list_display = ("name", "category")
+    list_filter = ("category",)
+    search_fields = ("name", "category__name")
+
+
+@admin.register(Group)
+class GroupAdmin(admin.ModelAdmin):
+    list_display = ("name", "url", "category")
+    list_filter = ("category",)
+    search_fields = ("name", "url", "category__name")
+
+
+@admin.register(Keyword)
+class KeywordAdmin(admin.ModelAdmin):
+    list_display = ("name", "category")
+    list_filter = ("category",)
+    search_fields = ("name", "category__name")
