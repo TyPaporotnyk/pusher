c1fbee7b61f3fb49805e1391bb9e16b3a974fa36
Daniil <39118675+TyPaporotnyk@users.noreply.github.com>
2024-06-15 18:04:19 +0300

    cahnged common url from retrive to list

--- apps/customers/urls.py
+++ apps/customers/urls.py
@@ -5,18 +5,16 @@ from . import views
 urlpatterns = [
     path("me/", views.CustomerView.as_view({"get": "retrieve", "patch": "partial_update"}), name="customer"),
     path("me/", views.UpdateCustomerView.as_view(), name="customer"),
-    path("groups/", views.CustomerGroupView.as_view({"get": "retrieve", "post": "create"}), name="customer-groups"),
-    path(
-        "keywords/", views.CustomerKeywordView.as_view({"get": "retrieve", "post": "create"}), name="customer-keywords"
-    ),
+    path("groups/", views.CustomerGroupView.as_view({"get": "list", "post": "create"}), name="customer-groups"),
+    path("keywords/", views.CustomerKeywordView.as_view({"get": "list", "post": "create"}), name="customer-keywords"),
     path(
         "categories/",
-        views.CustomerCategoryView.as_view({"get": "retrieve", "post": "create"}),
+        views.CustomerCategoryView.as_view({"get": "list", "post": "create"}),
         name="customer-categories",
     ),
     path(
         "blacklists/",
-        views.CustomerBlackListView.as_view({"get": "retrieve", "post": "create"}),
+        views.CustomerBlackListView.as_view({"get": "list", "post": "create"}),
         name="customer-blacklists",
     ),
     path("posts/", views.CustomerPostView.as_view({"get": "list"}), name="match-posts"),
