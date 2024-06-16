82710aede35feff4427abc9acfdbb2888a07b863
Daniil <39118675+TyPaporotnyk@users.noreply.github.com>
2024-06-16 04:30:39 +0300

    Added creation categoryif it is not exists

--- apps/common/serializers.py
+++ apps/common/serializers.py
@@ -1,6 +1,7 @@
 from rest_framework import serializers
 
 from apps.common.models import Blacklist, Category, Group, Keyword
+from apps.customers.services import CustomerService
 
 
 class CategorySerializer(serializers.ModelSerializer):
@@ -11,24 +12,60 @@ class CategorySerializer(serializers.ModelSerializer):
 
 
 class GroupSerializer(serializers.ModelSerializer):
-    category = CategorySerializer(read_only=True)
+    category = serializers.CharField(required=False)
 
     class Meta:
         model = Group
         fields = ["id", "url", "category", "is_active"]
 
+    def create(self, validated_data):
+        category_name = validated_data.get("category")
+
+        if category_name is not None:
+            category, _ = Category.objects.get_or_create(
+                name=category_name.strip(),
+                customer=CustomerService(self.context.get("request")).get_customer(),
+            )
+            validated_data["category"] = category
+
+        return super().create(validated_data)
+
 
 class KeywordSerializer(serializers.ModelSerializer):
-    category = CategorySerializer(read_only=True)
+    category = serializers.CharField(required=False)
 
     class Meta:
         model = Keyword
         fields = ["id", "name", "category", "is_active"]
 
+    def create(self, validated_data):
+        category_name = validated_data.get("category")
+
+        if category_name is not None:
+            category, _ = Category.objects.get_or_create(
+                name=category_name.strip(),
+                customer=CustomerService(self.context.get("request")).get_customer(),
+            )
+            validated_data["category"] = category
+
+        return super().create(validated_data)
+
 
 class BlacklistSerializer(serializers.ModelSerializer):
-    category = CategorySerializer(read_only=True)
+    category = serializers.CharField(required=False)
 
     class Meta:
         model = Blacklist
         fields = ["id", "name", "category", "is_active"]
+
+    def create(self, validated_data):
+        category_name = validated_data.get("category")
+
+        if category_name is not None:
+            category, _ = Category.objects.get_or_create(
+                name=category_name.strip(),
+                customer=CustomerService(self.context.get("request")).get_customer(),
+            )
+            validated_data["category"] = category
+
+        return super().create(validated_data)
