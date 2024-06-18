140715979293f5e0a6feab9fd28dc5a19f4b67d9
Daniil <39118675+TyPaporotnyk@users.noreply.github.com>
2024-06-18 14:45:55 +0300

    Fixed post image download error

--- apps/base/services/files.py
+++ apps/base/services/files.py
@@ -1,18 +1,23 @@
+import hashlib
+import os
 import tempfile
 
 import requests
-from django.core import files
+from django.core.files import File
 
 from apps.base.exceptions.files import DownloadFileException
 
 
-def download_file(url) -> tuple[str, files.File]:
+def download_file(url) -> tuple[str, File]:
     response = requests.get(url, stream=True)
     if response.status_code != requests.codes.ok:
         raise DownloadFileException
 
-    file_name = url.split("/")[-1]
-    lf = tempfile.NamedTemporaryFile()
+    hash_object = hashlib.md5(url.encode())
+    file_extension = os.path.splitext(url)[-1]
+    file_name = f"{hash_object.hexdigest()[:10]}{file_extension}"
+
+    lf = tempfile.NamedTemporaryFile(delete=False)
 
     for block in response.iter_content(1024 * 8):
         if not block:
@@ -20,4 +25,5 @@ def download_file(url) -> tuple[str, files.File]:
 
         lf.write(block)
 
-    return file_name, files.File(lf)
+    lf.seek(0)
+    return file_name, File(lf)
